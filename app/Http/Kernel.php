884f1e09318a77364b2ae0ba62150a61848a188a
kelvinpaulsirikwa <kelvinpaul374@gmail.com>
2026-01-18 13:19:08 +0300

    Update project

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,13 +39,13 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\SetLocale::class, 
-            \App\Http\Middleware\LogUniqueVisits::class,
-            \App\Http\Middleware\EnforcePasswordChange::class,
 
-            // SecureCookies temporarily disabled for debugging
+            // All other middleware disabled for debugging
+            // \App\Http\Middleware\VerifyCsrfToken::class,
+            // \App\Http\Middleware\SetLocale::class, 
+            // \App\Http\Middleware\LogUniqueVisits::class,
+            // \App\Http\Middleware\EnforcePasswordChange::class,
             // \App\Http\Middleware\SecureCookies::class,
 
         ],
