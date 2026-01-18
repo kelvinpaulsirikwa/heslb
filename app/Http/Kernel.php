065f4fad1f962acc68c253aa4cf7a8d210bcd094
kelvinpaulsirikwa <kelvinpaul374@gmail.com>
2026-01-18 11:08:01 +0300

    Update project

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,21 +35,21 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            // Custom Cookie Management System
-            \App\Http\Middleware\CustomCSRFMiddleware::class,
-            \App\Http\Middleware\CustomAuthMiddleware::class,
+            // Legacy middleware (temporarily restored to fix 500 error)
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SetLocale::class, 
             \App\Http\Middleware\LogUniqueVisits::class,
             \App\Http\Middleware\EnforcePasswordChange::class,
+            \App\Http\Middleware\SecureCookies::class,
 
-            // Legacy middleware (commented out - replaced by custom system)
-            // \App\Http\Middleware\EncryptCookies::class,
-            // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            // \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            // \App\Http\Middleware\VerifyCsrfToken::class,
-            // \App\Http\Middleware\SecureCookies::class,
+            // Custom Cookie Management System (temporarily disabled)
+            // \App\Http\Middleware\CustomCSRFMiddleware::class,
+            // \App\Http\Middleware\CustomAuthMiddleware::class,
 
         ],
 
